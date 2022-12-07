31b569c4609733943c855f594cf1a8f97e4038cd
Ichan <kfanuncio2@gmail.com>
2022-12-07 16:48:00 +0800

    sql

---
+++ DB/orders.sql
@@ -0,0 +1,220 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 07, 2022 at 09:47 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `orders`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `Name` varchar(250) CHARACTER SET latin1 NOT NULL,
+  `Email` varchar(250) CHARACTER SET latin1 NOT NULL,
+  `Mobile` varchar(250) CHARACTER SET latin1 NOT NULL,
+  `Subject` varchar(250) CHARACTER SET latin1 NOT NULL,
+  `Message` varchar(250) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `ID` int(11) NOT NULL,
+  `username` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `fullname` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `email` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `contact` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `address` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(30) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`ID`, `username`, `fullname`, `email`, `contact`, `address`, `password`) VALUES
+(1, 'akoako', 'ako', 'ako@gmail.com', '12345678901', 'siya', 'qwerty');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `ID` int(30) NOT NULL,
+  `name` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `price` int(30) NOT NULL,
+  `description` varchar(200) CHARACTER SET utf8 NOT NULL,
+  `images_path` varchar(200) CHARACTER SET utf8 NOT NULL,
+  `options` varchar(10) CHARACTER SET utf8 NOT NULL DEFAULT 'ENABLE'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`ID`, `name`, `price`, `description`, `images_path`, `options`) VALUES
+(1, 'burjer', 123, 'pagkain to', 'images/burger.jpg', 'ENABLE'),
+(2, 'ichan', 123, 'masarap siya pramis', 'images/burger.jpg', 'ENABLE');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manager`
+--
+
+CREATE TABLE `manager` (
+  `username` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `fullname` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `email` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `contact` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `address` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(30) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `manager`
+--
+
+INSERT INTO `manager` (`username`, `fullname`, `email`, `contact`, `address`, `password`) VALUES
+('musmus', 'Kristian', 'kfanuncio2@gmail.com', '09381474797', 'taguig ', '1234567890'),
+('musmusmus', 'mus', 'musmus@gmail.com', '09381474797', 'bulacan', 'qwertyuiop');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `ID` int(30) NOT NULL,
+  `item_ID` int(30) NOT NULL,
+  `total_price` double NOT NULL,
+  `quantity` int(30) NOT NULL,
+  `createdOn` date NOT NULL DEFAULT current_timestamp(),
+  `customer_ID` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`ID`, `item_ID`, `total_price`, `quantity`, `createdOn`, `customer_ID`) VALUES
+(1, 1, 123, 1, '2022-11-26', 1),
+(2, 1, 123, 2, '2022-11-26', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `store`
+--
+
+CREATE TABLE `store` (
+  `R_ID` int(30) NOT NULL,
+  `name` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `email` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `contact` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `address` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `M_ID` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `verified` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `store`
+--
+
+INSERT INTO `store` (`R_ID`, `name`, `email`, `contact`, `address`, `M_ID`, `verified`) VALUES
+(1, 'AntStore', 'antatore@gmail.com', '09381474797', 'taguig ', 'musmus', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `manager`
+--
+ALTER TABLE `manager`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `F_ID` (`item_ID`,`customer_ID`),
+  ADD KEY `item_ID` (`item_ID`);
+
+--
+-- Indexes for table `store`
+--
+ALTER TABLE `store`
+  ADD PRIMARY KEY (`R_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `ID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `ID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `store`
+--
+ALTER TABLE `store`
+  MODIFY `R_ID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
